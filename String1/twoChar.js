79cce6cbc2970af4a99305725819de8e24cc25cb
Matthew De Andrade <fayrfax@gmail.com>
2025-01-07 14:11:24 -0500

    conCat added.

---
+++ String1/twoChar.js
@@ -0,0 +1,15 @@
+//** Given a string and an index,
+// return a string length 2 starting at the given index.
+//  If the index is too big or too small to define a string length 2,
+// use the first 2 chars. The string length will be at least 2.
+/** Examples
+
+twoChar('java', 0) → ja
+twoChar('java', 2) → va
+twoChar('java', 3) → ja **/
+
+function twoChar(str, index) {}
+
+console.log(twoChar("java", 0));
+console.log(twoChar("java", 2));
+console.log(twoChar("java", 3));
