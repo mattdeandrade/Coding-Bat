86ef5221effc83a873314faff7617d8fe08a7ca3
Matthew De Andrade <fayrfax@gmail.com>
2025-01-12 13:57:21 -0500

    String 2 coding bat exercise files added.

---
+++ String2/doubleChar.js
@@ -0,0 +1,22 @@
+/** Given a string, return a string where for every char in the original, there are two chars.
+
+Examples
+
+doubleChar('The') → TThhee
+doubleChar('AAbb') → AAAAbbbb
+doubleChar('Hi-There') → HHii--TThheerree*/
+
+function doubleChar(str) {
+  const newArray = str.split("");
+  const newStr = "";
+  const size = str.length;
+
+  for (let i = 0; i < size; i++) {
+    newStr = newStr + str.charAt(i) + str.charAt(i);
+  }
+  return newStr;
+}
+
+console.log(doubleChar("The"));
+console.log(doubleChar("a"));
+console.log(doubleChar(""));
